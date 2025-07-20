82eb4e202b19ce0034448e4304a8e880a9f1adcf
AyushiSingh-1 <148997728+AyushiSingh-1@users.noreply.github.com>
2025-07-21 01:15:49 +0530

    updated database link

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const connect = mongoose.connect("mongodb://localhost:27017/login");
+const connect = mongoose.connect("process.env.MONGO_URI");
 
 connect.then(()=> {
     console.log("Database connected Successfully");
