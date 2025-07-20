174b24097be5d92368e41a840b949d5ff3b000cb
AyushiSingh-1 <148997728+AyushiSingh-1@users.noreply.github.com>
2025-07-21 01:49:41 +0530

    changed ports

--- src/index.js
+++ src/index.js
@@ -67,7 +67,5 @@ app.post("/login", async (req, res) => {
     }
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port,()=> {
-    console.log('Server running on port:  ${port}');
-})
+
+module.exports = app;
