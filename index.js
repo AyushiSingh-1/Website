8d023ab85802b6bf9c8fdb49069d5a0a1884ed87
AyushiSingh-1 <148997728+AyushiSingh-1@users.noreply.github.com>
2025-07-21 23:34:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,71 +1,77 @@
-const express = require ('express');
-const path = require("path");
-const bcrypt =  require("bcrypt");
+const express = require('express');
+const path = require("path"); // Make sure path is required
+const bcrypt = require("bcrypt");
 const collection = require("./config"); 
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use(express.urlencoded({extended : false}));
+// Set the view engine to EJS
 app.set('view engine', 'ejs');
 
-app.use(express.static('public'));
+// THIS IS THE NEW LINE TO FIX THE ERROR
+// It tells Express where to find your views folder
+app.set('views', path.join(__dirname, 'views'));
 
-app.get("/", (req,res) =>
-{  res.render("home");
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.get("/", (req, res) => {
+    res.render("home");
 });
 
-app.get("/login", (req,res) =>
-{  res.render("login");
-
+app.get("/login", (req, res) => {
+    res.render("login");
 });
 
-app.get("/signup", (req,res) =>
-{  res.render("signup");
-
+app.get("/signup", (req, res) => {
+    res.render("signup");
 });
 
 app.post("/signup", async (req, res) => {
-    const existingUser = await collection.findOne({ name: req.body.firstname });
-    if (existingUser) {
-        return res.send("User already exists. Please choose a different username.");
+    try {
+        const existingUser = await collection.findOne({ name: req.body.firstname });
+        if (existingUser) {
+            return res.send("User already exists. Please choose a different username.");
+        }
+
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
+
+        const data = {
+            name: req.body.firstname,
+            password: hashedPassword
+        };
+
+        await collection.insertMany(data);
+        res.render("login");
+    } catch (error) {
+        console.log(error);
+        res.send("An error occurred during signup.");
     }
-
-    // Hash the password
-    const saltRounds = 10;
-    const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
-
-    const data = {
-        name: req.body.firstname,
-        password: hashedPassword // Save the HASHED password
-    }
-
-    await collection.insertMany(data);
-    res.render("login");
 });
 
 app.post("/login", async (req, res) => {
     try {
         const check = await collection.findOne({ name: req.body.firstname });
         if (!check) {
-            // Use res to send a response
-            return res.send("wrong details"); 
+            return res.send("Wrong username or password");
         }
         const isPasswordMatch = await bcrypt.compare(req.body.password, check.password);
         if (!isPasswordMatch) {
-            // Use res here as well
-            return res.send("wrong password");
+            return res.send("Wrong username or password");
         }
-        // If login is successful, you should probably render a user dashboard or the homepage
-        res.render("home"); // Or a different page for logged-in users
-
-    } catch (error) { // It's good practice to catch the specific error
+        res.render("home");
+    } catch (error) {
         console.log(error);
-        res.send("An error occurred");
+        res.send("An error occurred during login.");
     }
 });
-
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
 
 module.exports = app;
